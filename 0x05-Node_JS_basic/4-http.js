9c6d2c909ae8efb01fd5103fe40c74eeba452fe3
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-11-29 02:44:17 +0000

    four

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,6 +3,7 @@ const http = require('http');
 const port = 1245;
 const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
+  res.statusCode = 200;
   res.end();
 });
 
